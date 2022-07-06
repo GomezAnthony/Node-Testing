2402a395118df053a2338dd8c64fefce8b36def5
Anthony Gomez <gomez9162@gmail.com>
2022-07-05 22:40:01 -0500

    node testing

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose')
-const Blog = require('./models/blog');
-const { result } = require('lodash');
+const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
@@ -20,11 +19,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for request
-
-
 // middleware and static files
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
@@ -37,19 +34,8 @@ app.get('/about', (req, res) => {
   res.render('about', {title: 'About'})
 });
 
-// Blog Routes
-app.get('/blogs', (req, res) => {
-   Blog.find().sort({ createdAt: -1 })
-   .then((result) => {
-      res.render('index', {title: 'All blogs', blogs: result})
-   })
-   .catch((err) => {
-      console.log(err)
-   })
-})
-app.get('/blogs/create', (req, res) => {
-   res.render('create', {title: 'Create'})
-})
+app.use('/blogs', blogRoutes);
+
 
 app.use((req, res) =>{
    res.status(404).render('404', {title: '404'})
